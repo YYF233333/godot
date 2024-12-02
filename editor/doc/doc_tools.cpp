36fdedc2b48d73e66cd8317089628472cdd2a62c
Yufeng Ying <nbyyf2002@mail.ustc.edu.cn>
2024-12-03 02:00:33 +0800

    All changes from branch list, stack_list, reduce_list, image_ext_list and save_ext.

--- editor/doc/doc_tools.cpp
+++ editor/doc/doc_tools.cpp
@@ -1250,7 +1250,7 @@ Error DocTools::erase_classes(const String &p_dir) {
 		return err;
 	}
 
-	List<String> to_erase;
+	LocalVector<String> to_erase;
 
 	da->list_dir_begin();
 	String path;
@@ -1263,9 +1263,8 @@ Error DocTools::erase_classes(const String &p_dir) {
 	}
 	da->list_dir_end();
 
-	while (to_erase.size()) {
-		da->remove(to_erase.front()->get());
-		to_erase.pop_front();
+	for (String &E : to_erase) {
+		da->remove(E);
 	}
 
 	return OK;
