1458ac14412db38db754ce7b04ebd4be32481dad
Yufeng Ying <nbyyf2002@mail.ustc.edu.cn>
2024-12-03 02:00:33 +0800

    All changes from branch list, stack_list, reduce_list, image_ext_list and save_ext.

--- editor/import/resource_importer_svg.h
+++ editor/import/resource_importer_svg.h
@@ -31,6 +31,7 @@
 #pragma once
 
 #include "core/io/resource_importer.h"
+#include "core/templates/local_vector.h"
 
 class ResourceImporterSVG : public ResourceImporter {
 	GDCLASS(ResourceImporterSVG, ResourceImporter);
@@ -38,7 +39,7 @@ class ResourceImporterSVG : public ResourceImporter {
 public:
 	virtual String get_importer_name() const override;
 	virtual String get_visible_name() const override;
-	virtual void get_recognized_extensions(List<String> *p_extensions) const override;
+	virtual void get_recognized_extensions(LocalVector<String> &p_extensions) const override;
 	virtual String get_save_extension() const override;
 	virtual String get_resource_type() const override;
 
