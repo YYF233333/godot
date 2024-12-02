de4c0fe0ba55b7fe52792187f0ba1d63d0fdb0f4
Yufeng Ying <nbyyf2002@mail.ustc.edu.cn>
2024-12-03 02:00:33 +0800

    All changes from branch list, stack_list, reduce_list, image_ext_list and save_ext.

--- editor/import/3d/resource_importer_obj.h
+++ editor/import/3d/resource_importer_obj.h
@@ -36,7 +36,7 @@ class EditorOBJImporter : public EditorSceneFormatImporter {
 	GDCLASS(EditorOBJImporter, EditorSceneFormatImporter);
 
 public:
-	virtual void get_extensions(List<String> *r_extensions) const override;
+	virtual void get_extensions(LocalVector<String> &r_extensions) const override;
 	virtual Node *import_scene(const String &p_path, uint32_t p_flags, const HashMap<StringName, Variant> &p_options, List<String> *r_missing_deps, Error *r_err = nullptr) override;
 };
 
@@ -46,7 +46,7 @@ class ResourceImporterOBJ : public ResourceImporter {
 public:
 	virtual String get_importer_name() const override;
 	virtual String get_visible_name() const override;
-	virtual void get_recognized_extensions(List<String> *p_extensions) const override;
+	virtual void get_recognized_extensions(LocalVector<String> &p_extensions) const override;
 	virtual String get_save_extension() const override;
 	virtual String get_resource_type() const override;
 	virtual int get_format_version() const override;
