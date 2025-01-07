41a6e709ac4d82e187b8e33f395305059ade8671
Yufeng Ying <nbyyf2002@mail.ustc.edu.cn>
2025-01-07 15:59:02 +0800

    Change Object::get_argument_options.

--- scene/resources/sprite_frames.h
+++ scene/resources/sprite_frames.h
@@ -104,7 +104,7 @@ public:
 	void clear_all();
 
 #ifdef TOOLS_ENABLED
-	virtual void get_argument_options(const StringName &p_function, int p_idx, List<String> *r_options) const override;
+	virtual void get_argument_options(const StringName &p_function, int p_idx, LocalVector<String> &r_options) const override;
 #endif
 
 	SpriteFrames();
