3ec07b2af3557f74eb73cb68aefc606c49a89c3b
Yufeng Ying <nbyyf2002@mail.ustc.edu.cn>
2025-01-17 19:11:46 +0800

    Change GDScriptTextDocument::find_symbols.

--- modules/gdscript/language_server/gdscript_text_document.cpp
+++ modules/gdscript/language_server/gdscript_text_document.cpp
@@ -415,7 +415,7 @@ Variant GDScriptTextDocument::hover(const Dictionary &p_params) {
 Array GDScriptTextDocument::definition(const Dictionary &p_params) {
 	LSP::TextDocumentPositionParams params;
 	params.load(p_params);
-	List<const LSP::DocumentSymbol *> symbols;
+	LocalVector<const LSP::DocumentSymbol *> symbols;
 	Array arr = find_symbols(params, symbols);
 	return arr;
 }
@@ -423,10 +423,10 @@ Array GDScriptTextDocument::definition(const Dictionary &p_params) {
 Variant GDScriptTextDocument::declaration(const Dictionary &p_params) {
 	LSP::TextDocumentPositionParams params;
 	params.load(p_params);
-	List<const LSP::DocumentSymbol *> symbols;
+	LocalVector<const LSP::DocumentSymbol *> symbols;
 	Array arr = find_symbols(params, symbols);
-	if (arr.is_empty() && !symbols.is_empty() && !symbols.front()->get()->native_class.is_empty()) { // Find a native symbol
-		const LSP::DocumentSymbol *symbol = symbols.front()->get();
+	if (arr.is_empty() && !symbols.is_empty() && !symbols[0]->native_class.is_empty()) { // Find a native symbol
+		const LSP::DocumentSymbol *symbol = symbols[0];
 		if (GDScriptLanguageProtocol::get_singleton()->is_goto_native_symbols_enabled()) {
 			String id;
 			switch (symbol->kind) {
@@ -488,7 +488,7 @@ void GDScriptTextDocument::show_native_symbol_in_editor(const String &p_symbol_i
 	DisplayServer::get_singleton()->window_move_to_foreground();
 }
 
-Array GDScriptTextDocument::find_symbols(const LSP::TextDocumentPositionParams &p_location, List<const LSP::DocumentSymbol *> &r_list) {
+Array GDScriptTextDocument::find_symbols(const LSP::TextDocumentPositionParams &p_location, LocalVector<const LSP::DocumentSymbol *> &r_list) {
 	Array arr;
 	const LSP::DocumentSymbol *symbol = GDScriptLanguageProtocol::get_singleton()->get_workspace()->resolve_symbol(p_location);
 	if (symbol) {
