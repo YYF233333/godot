14fe056939281ed9f960ce26084bb27fef4e2709
Yufeng Ying <nbyyf2002@mail.ustc.edu.cn>
2025-01-17 19:09:18 +0800

    Change ExtendGDScriptParser::get_document_links.

--- modules/gdscript/language_server/gdscript_extend_parser.cpp
+++ modules/gdscript/language_server/gdscript_extend_parser.cpp
@@ -856,7 +856,7 @@ const LSP::DocumentSymbol *ExtendGDScriptParser::get_member_symbol(const String 
 	return nullptr;
 }
 
-const List<LSP::DocumentLink> &ExtendGDScriptParser::get_document_links() const {
+const LocalVector<LSP::DocumentLink> &ExtendGDScriptParser::get_document_links() const {
 	return document_links;
 }
 
