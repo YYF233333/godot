7f0b54c8b3f1bf09a1f18504816395ead741b799
Yyf2333 <2514537033@qq.com>
2025-01-20 16:08:31 +0800

    Change ResourceFormatImporter::get_importers_for_extension.

--- core/io/resource_importer.cpp
+++ core/io/resource_importer.cpp
@@ -472,13 +472,13 @@ void ResourceFormatImporter::add_importer(const Ref<ResourceImporter> &p_importe
 	}
 }
 
-void ResourceFormatImporter::get_importers_for_extension(const String &p_extension, List<Ref<ResourceImporter>> *r_importers) {
+void ResourceFormatImporter::get_importers_for_extension(const String &p_extension, LocalVector<Ref<ResourceImporter>> &r_importers) {
 	for (int i = 0; i < importers.size(); i++) {
 		LocalVector<String> local_exts;
 		importers[i]->get_recognized_extensions(local_exts);
 		for (const String &F : local_exts) {
 			if (p_extension.to_lower() == F) {
-				r_importers->push_back(importers[i]);
+				r_importers.push_back(importers[i]);
 				break;
 			}
 		}
