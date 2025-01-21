9d5a6be3a4764b30311dff9f8e8483c17e82e71d
Yyf2333 <2514537033@qq.com>
2025-01-21 15:59:45 +0800

    Change ResourceImporter::get_import_options and related.

--- editor/import/3d/post_import_plugin_skeleton_renamer.h
+++ editor/import/3d/post_import_plugin_skeleton_renamer.h
@@ -36,7 +36,7 @@ class PostImportPluginSkeletonRenamer : public EditorScenePostImportPlugin {
 	GDCLASS(PostImportPluginSkeletonRenamer, EditorScenePostImportPlugin);
 
 public:
-	virtual void get_internal_import_options(InternalImportCategory p_category, List<ResourceImporter::ImportOption> *r_options) override;
+	virtual void get_internal_import_options(InternalImportCategory p_category, LocalVector<ResourceImporter::ImportOption> &r_options) override;
 	virtual void internal_process(InternalImportCategory p_category, Node *p_base_scene, Node *p_node, Ref<Resource> p_resource, const Dictionary &p_options) override;
 
 	void _internal_process(InternalImportCategory p_category, Node *p_base_scene, Node *p_node, Ref<Resource> p_resource, const Dictionary &p_options, const HashMap<String, String> &p_rename_map);
