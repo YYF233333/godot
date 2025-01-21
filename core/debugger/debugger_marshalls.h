0ec447426bf79f6911bf03c1a2626bb3fe10f11e
Yyf2333 <2514537033@qq.com>
2025-01-21 22:45:47 +0800

    Small cleanup in Debugger.

--- core/debugger/debugger_marshalls.h
+++ core/debugger/debugger_marshalls.h
@@ -45,7 +45,7 @@ struct DebuggerMarshalls {
 	};
 
 	struct ScriptStackDump {
-		List<ScriptLanguage::StackInfo> frames;
+		LocalVector<ScriptLanguage::StackInfo> frames;
 		ScriptStackDump() {}
 
 		Array serialize();
