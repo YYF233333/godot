2b5b9f287463e79ecba5ad3d3e480f978c341a1f
Yyf2333 <2514537033@qq.com>
2025-01-21 15:59:45 +0800

    Change ResourceImporter::get_import_options and related.

--- editor/import/resource_importer_csv_translation.cpp
+++ editor/import/resource_importer_csv_translation.cpp
@@ -67,9 +67,9 @@ String ResourceImporterCSVTranslation::get_preset_name(int p_idx) const {
 	return "";
 }
 
-void ResourceImporterCSVTranslation::get_import_options(const String &p_path, List<ImportOption> *r_options, int p_preset) const {
-	r_options->push_back(ImportOption(PropertyInfo(Variant::BOOL, "compress"), true));
-	r_options->push_back(ImportOption(PropertyInfo(Variant::INT, "delimiter", PROPERTY_HINT_ENUM, "Comma,Semicolon,Tab"), 0));
+void ResourceImporterCSVTranslation::get_import_options(const String &p_path, LocalVector<ImportOption> &r_options, int p_preset) const {
+	r_options.push_back(ImportOption(PropertyInfo(Variant::BOOL, "compress"), true));
+	r_options.push_back(ImportOption(PropertyInfo(Variant::INT, "delimiter", PROPERTY_HINT_ENUM, "Comma,Semicolon,Tab"), 0));
 }
 
 Error ResourceImporterCSVTranslation::import(ResourceUID::ID p_source_id, const String &p_source_file, const String &p_save_path, const HashMap<StringName, Variant> &p_options, List<String> *r_platform_variants, List<String> *r_gen_files, Variant *r_metadata) {
