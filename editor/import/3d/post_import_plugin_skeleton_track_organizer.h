0a0c6ffb48c0c581832b22db3ec97587b300ac33
Yyf2333 <2514537033@qq.com>
2025-01-21 15:59:45 +0800

    Change ResourceImporter::get_import_options and related.

--- editor/import/3d/post_import_plugin_skeleton_track_organizer.h
+++ editor/import/3d/post_import_plugin_skeleton_track_organizer.h
@@ -36,6 +36,6 @@ class PostImportPluginSkeletonTrackOrganizer : public EditorScenePostImportPlugi
 	GDCLASS(PostImportPluginSkeletonTrackOrganizer, EditorScenePostImportPlugin);
 
 public:
-	virtual void get_internal_import_options(InternalImportCategory p_category, List<ResourceImporter::ImportOption> *r_options) override;
+	virtual void get_internal_import_options(InternalImportCategory p_category, LocalVector<ResourceImporter::ImportOption> &r_options) override;
 	virtual void internal_process(InternalImportCategory p_category, Node *p_base_scene, Node *p_node, Ref<Resource> p_resource, const Dictionary &p_options) override;
 };
