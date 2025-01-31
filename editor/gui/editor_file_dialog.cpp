d94929dfee58c00aec025f17a5064f6bba562748
Yyf2333 <2514537033@qq.com>
2025-01-31 16:47:28 +0800

    Change a local variable.

--- editor/gui/editor_file_dialog.cpp
+++ editor/gui/editor_file_dialog.cpp
@@ -1049,7 +1049,7 @@ void EditorFileDialog::update_file_list() {
 	dir_access->list_dir_begin();
 
 	LocalVector<FileInfo> file_infos;
-	List<String> dirs;
+	LocalVector<String> dirs;
 
 	String item = dir_access->get_next();
 
@@ -1124,9 +1124,7 @@ void EditorFileDialog::update_file_list() {
 		}
 	}
 
-	while (!dirs.is_empty()) {
-		const String &dir_name = dirs.front()->get();
-
+	for (const String &dir_name : dirs) {
 		bool bundle = dir_access->is_bundle(dir_name);
 		bool found = true;
 		if (bundle) {
@@ -1158,8 +1156,6 @@ void EditorFileDialog::update_file_list() {
 			item_list->set_item_metadata(-1, d);
 			item_list->set_item_icon_modulate(-1, get_dir_icon_color(String(d["path"])));
 		}
-
-		dirs.pop_front();
 	}
 
 	for (const FileInfo &file_info : file_infos) {
