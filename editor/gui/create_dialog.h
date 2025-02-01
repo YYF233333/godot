fefb4f580afca96ffe8f68f1bde61da96dffe5c4
Yyf2333 <2514537033@qq.com>
2025-02-01 14:11:25 +0800

    Cleanup CreateDialog.

--- editor/gui/create_dialog.h
+++ editor/gui/create_dialog.h
@@ -69,7 +69,7 @@ class CreateDialog : public ConfirmationDialog {
 	HashMap<String, TreeItem *> search_options_types;
 	HashMap<String, String> custom_type_parents;
 	HashMap<String, int> custom_type_indices;
-	List<TypeInfo> type_info_list;
+	LocalVector<TypeInfo> type_info_list;
 	HashSet<StringName> type_blacklist;
 	HashSet<StringName> custom_type_blocklist;
 
