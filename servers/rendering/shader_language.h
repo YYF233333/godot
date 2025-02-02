7a0657a895c36d4ac0de0f3da9e2d127d08005c1
Yyf2333 <2514537033@qq.com>
2025-02-02 14:54:35 +0800

    Change ShaderLanguage::get_keyword_list.

--- servers/rendering/shader_language.h
+++ servers/rendering/shader_language.h
@@ -857,7 +857,7 @@ public:
 	static uint32_t get_datatype_size(DataType p_type);
 	static uint32_t get_datatype_component_count(DataType p_type);
 
-	static void get_keyword_list(List<String> *r_keywords);
+	static LocalVector<String> get_keyword_list();
 	static bool is_control_flow_keyword(String p_keyword);
 	static void get_builtin_funcs(List<String> *r_keywords);
 
