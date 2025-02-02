5920cdc71e285fc55272f9febaebd88b151c19fa
Yyf2333 <2514537033@qq.com>
2025-02-02 15:44:51 +0800

    Cleanup CanvasItemEditor.

--- editor/plugins/canvas_item_editor_plugin.cpp
+++ editor/plugins/canvas_item_editor_plugin.cpp
@@ -4721,7 +4721,7 @@ void CanvasItemEditor::_popup_callback(int p_op) {
 
 		} break;
 		case ANIM_PASTE_POSE: {
-			if (!pose_clipboard.size()) {
+			if (pose_clipboard.is_empty()) {
 				break;
 			}
 
